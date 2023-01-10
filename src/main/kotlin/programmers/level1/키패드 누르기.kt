70f3aa8b4212ce9b42bac677ec88a82ac3e7eac2
TaegeunYou <dbxorms2310@naver.com>
2023-01-10 22:02:15 +0900

    011001 refactoring

--- src/main/kotlin/programmers/level1/키패드 누르기.kt
+++ src/main/kotlin/programmers/level1/키패드 누르기.kt
@@ -5,16 +5,10 @@ class Solution011001 {
         var right = Pair(0, 0)
         var left = Pair(2, 0)
         val positionMap = mapOf(
-            1 to Pair(0, 3),
-            2 to Pair(1, 3),
-            3 to Pair(2, 3),
-            4 to Pair(0, 2),
-            5 to Pair(1, 2),
-            6 to Pair(2, 2),
-            7 to Pair(0, 1),
-            8 to Pair(1, 1),
-            9 to Pair(2, 1),
-            0 to Pair(1, 0),
+            1 to Pair(0, 3), 2 to Pair(1, 3), 3 to Pair(2, 3),
+            4 to Pair(0, 2), 5 to Pair(1, 2), 6 to Pair(2, 2),
+            7 to Pair(0, 1), 8 to Pair(1, 1), 9 to Pair(2, 1),
+                             0 to Pair(1, 0),
         )
         return numbers.joinToString("") {
             when (it) {
@@ -30,7 +24,7 @@ class Solution011001 {
                     val target = positionMap[it]!!
                     val leftDistance = Math.abs(target.first - left.first) + Math.abs(target.second - left.second)
                     val rightDistance = Math.abs(target.first - right.first) + Math.abs(target.second - right.second)
-                    if ((leftDistance == rightDistance && hand == "left") || rightDistance > leftDistance) {
+                    if (rightDistance > leftDistance || (leftDistance == rightDistance && hand == "left")) {
                         left = target
                         "L"
                     } else {
