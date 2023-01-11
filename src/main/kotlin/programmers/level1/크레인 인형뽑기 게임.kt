24efd5dd77f7bb348d5db228030c025e2dc4218b
TaegeunYou <dbxorms2310@naver.com>
2023-01-11 22:20:39 +0900

    0111

---
+++ src/main/kotlin/programmers/level1/크레인 인형뽑기 게임.kt
@@ -0,0 +1,25 @@
+package programmers.level1
+
+class Solution011101 {
+    fun solution(board: Array<IntArray>, moves: IntArray): Int {
+        val box = (board.lastIndex downTo 0).map { listIdx ->
+            board.map {
+                it[listIdx]
+            }.filter {
+                it != 0
+            }.reversed().toMutableList()
+        }.reversed().toMutableList()
+        val newBox = ArrayDeque<Int>()
+        var answer = 0
+        moves.forEach { doll ->
+            box[doll-1].lastOrNull()?.also {
+                box[doll-1].removeLast()
+                if (newBox.lastOrNull() == it) {
+                    newBox.removeLast()
+                    answer += 2
+                } else newBox.addLast(it)
+            }
+        }
+        return answer
+    }
+}
