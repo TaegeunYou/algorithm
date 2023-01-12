3cb27e27a796eaec20332e47980b10771e90323c
TaegeunYou <66227661+TaegeunYou@users.noreply.github.com>
2023-01-12 23:55:38 +0900

    0112

---
+++ src/main/kotlin/programmers/level1/신규 아이디 추천.kt
@@ -0,0 +1,35 @@
+package programmers.level1
+
+class Solution011201 {
+    fun solution(new_id: String): String {
+        return new_id.toLowerCase()
+            .filter {
+                it.isLowerCase() || it.isDigit() || it == '-' || it == '_' || it == '.'
+            }
+            .replace("..*.".toRegex(), ".")
+            .let {
+                if (it.firstOrNull() == '.') {
+                    it.substring(1..it.lastIndex)
+                } else it
+                if (it.lastOrNull() == '.') {
+                    it.substring(0..it.lastIndex - 1)
+                } else it
+            }.let {
+                it.ifEmpty {
+                    "a"
+                }
+            }.let {
+                if (it.length >= 16) {
+                    it.substring(0..14).let {
+                        if (it.lastOrNull() == '.') {
+                            it.substring(0..it.lastIndex - 1)
+                        } else it
+                    }
+                } else it
+            }.let {
+                if (it.length <= 2) {
+                    it + it.last().toString().repeat(3 - it.length)
+                } else it
+            }
+    }
+}
