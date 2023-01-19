2e378418bb15f1ef9c5940e9f4e0314d07e24dfd
TaegeunYou <dbxorms2310@naver.com>
2023-01-19 22:41:03 +0900

    name change

--- src/main/kotlin/programmers/level1/K번째수.kt
+++ src/main/kotlin/programmers/level1/K번째수.kt
@@ -1,6 +1,6 @@
 package programmers.level1
 
-class Solution {
+class Solution011801 {
     fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
         return commands.map {
             array.slice(it[0]-1..it[1]-1).sorted()[it[2]-1]
