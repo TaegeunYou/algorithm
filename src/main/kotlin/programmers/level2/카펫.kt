6529a230579945116e6b2f12b1b7935b873ee3b6
TaegeunYou <dbxorms2310@naver.com>
2023-01-21 23:35:32 +0900

    012101 refactoring

--- src/main/kotlin/programmers/level2/카펫.kt
+++ src/main/kotlin/programmers/level2/카펫.kt
@@ -2,15 +2,10 @@ package programmers.level2
 
 class Solution012101 {
     fun solution(brown: Int, yellow: Int): IntArray {
-        val sum = 2 * brown - 4
-        val multi = brown + yellow
-
         val a = 1
         val b = -((brown - 4)/2 + 4)
-        val c = (brown + yellow)
-        println("$a $b $c")
-
+        val c = brown + yellow
         val column = ((-b + Math.sqrt((b * b - 4 * a * c).toDouble()))/ (2 * a)).toInt()
-        return intArrayOf(column, sum - column)
+        return intArrayOf(column, c/column)
     }
 }
