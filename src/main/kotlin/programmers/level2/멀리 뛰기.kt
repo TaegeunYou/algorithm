43b4e70bac1da9e724f11f7f2b15aad8ee1a7b55
TaegeunYou <dbxorms2310@naver.com>
2023-01-22 23:07:27 +0900

    0122

---
+++ src/main/kotlin/programmers/level2/멀리 뛰기.kt
@@ -0,0 +1,22 @@
+package programmers.level2
+
+import java.math.BigDecimal
+
+class Solution {
+    fun solution(n: Int): Long {
+        return (0..n/2).sumOf {
+            val numTwo = it
+            val numOne = (n-it*2)
+            factorial(numOne + numTwo) / (factorial(numOne) * factorial(numTwo))
+        }.rem(1234567.toBigDecimal()).toLong()
+    }
+    private fun factorial(num: Int): BigDecimal {
+        return (1..num).fold(1.toBigDecimal()) { total, i ->
+            total.multiply(i.toBigDecimal())
+        }
+    }
+}
+
+fun main() {
+    println(Solution().solution(4))
+}
