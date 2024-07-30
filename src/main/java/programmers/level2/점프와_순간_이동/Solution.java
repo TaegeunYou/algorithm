66919c31823074573d6c68b9e7b3c45778708971
TaegeunYou <dbxorms2310@naver.com>
2024-07-30 18:37:52 +0900

    0730

--- src/main/java/programmers/level2/점프와_순간_이동/Solution.java
+++ src/main/java/programmers/level2/점프와_순간_이동/Solution.java
@@ -3,9 +3,14 @@ package main.java.programmers.level2.점프와_순간_이동;
 public class Solution {
     public int solution(int n) {
         int ans = 0;
-
-        
-
-        return ans;
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n -= 1;
+                ans++;
+            }
+        }
+        return ++ans;
     }
 }
