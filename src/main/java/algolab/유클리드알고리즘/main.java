0a5fd0eb841cf34f9ed94b1e6ba8dc10c25eb400
TaegeunYou <dbxorms2310@naver.com>
2024-09-26 17:06:19 +0900

    0926

---
+++ src/main/java/algolab/유클리드알고리즘/main.java
@@ -0,0 +1,37 @@
+package main.java.algolab.유클리드알고리즘;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class main {
+
+    private void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int result = gcd(a, b);
+            sb.append(result).append("\n");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+
+        br.close();
+        System.out.println(sb);
+    }
+
+    private int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new main().solution();
+    }
+
+}
