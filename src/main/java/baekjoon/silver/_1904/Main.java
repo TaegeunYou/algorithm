40be9c599f1871512279ce34b09de28b56e8aaed
TaegeunYou <dbxorms2310@naver.com>
2025-08-04 13:09:42 +0900

    0804

---
+++ src/main/java/baekjoon/silver/_1904/Main.java
@@ -0,0 +1,24 @@
+package main.java.baekjoon.silver._1904;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[1000001];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+            dp[i] %= 15746;
+        }
+        System.out.println(dp[n]);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+}
